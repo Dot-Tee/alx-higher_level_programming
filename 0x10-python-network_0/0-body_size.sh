3c4b026edc6be30af89b5559c58e5b63a6dc99e8
Taiwo <taiwo1695@gmail.com>
2024-01-25 14:00:53 -0800

    bash script using curl

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # To get the content length and response size
-curl -sI "$1" | grep -i "Content-Length" | awk '{print "Size of the response body: " $2 " bytes"}'
+curl -sI "$1" | awk '{print "Size of the response body: " $2 " bytes"}'
---
+++ 0x10-python-network_0/1-body.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# It takes URL, sends a GET request to the URL and displays body of the response
+curl -Lsf "$1"
